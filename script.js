05690cb20b4b0eb50f1434ffca52f58d9b7a5421
Max Oliveira <max-oliveira@outlook.com.br>
2022-07-31 19:08:48 -0300

    feat: popolation modal

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const c = (el)=> document.querySelector(el);
-const cs = (el)=> document.querySelector(el);
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelector(el);
 
-pizzaJson.map((item,index)=>{
+pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
 
-        c('.pizzaWindowArea').style.opacity ='0';
-        c('.pizzaWindowArea').style.display ='flex';
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
-        setTimeout(() => { c('.pizzaWindowArea').style.opacity = '1'; },200)
-        
+        c('.pizzaBig img').src = pizzaJson[key].img
+       c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+       c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+       
+
+        c('.pizzaWindowArea').style.opacity = '0';
+        c('.pizzaWindowArea').style.display = 'flex';
+
+        setTimeout(() => { c('.pizzaWindowArea').style.opacity = '1'; }, 200)
+
     })
     c('.pizza-area').append(pizzaItem);
 })
